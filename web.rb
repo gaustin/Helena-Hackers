ca1d0c31bb31113d222260e0158a9b9e0cccd6f5
Grant Austin <gaustin@gmail.com>
2012-04-07 12:57:35 -0600

    premature abstraction

--- web.rb
+++ web.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, world"
+  'Hello, world'
+end
+
+get '/codeandcoffee' do
+  send_public('codeandcoffee.html')
+end
+
+def send_public(filename)
+  send_file(public_path(filename))
+end
+
+def public_path(filename)
+  File.expand_path(filename,
+                   settings.public_folder)
 end
